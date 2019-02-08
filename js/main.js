841e666d0cb0329fb6b85a7b4a43b3d0b66e74e8
alexkolotukhina <alex_kolotukhina@outlook.com>
2019-02-09 01:20:39 +0200

    add slider

---
+++ js/main.js
@@ -0,0 +1,23 @@
+(function($){
+
+	$(document).ready(function(){
+	  $('.slider-wrapper').slick({
+		  dots: true,
+		  arrows: false,
+		  customPaging : function(slider, i) {
+			var thumb = $(slider.$slides[i]).data();
+
+			if(i<9) {
+				return '0'+(i+1);
+			} else {
+				return i+1;
+			}
+		},
+		  speed: 500,
+		  autoplay: true,
+    	  autoplaySpeed: 3000
+		});
+
+	});
+
+})(jQuery);
